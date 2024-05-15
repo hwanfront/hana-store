0d340e6b4d3b826c3f60b58b8870eb2cd0985cfe
hwanfront <kim015jh@gmail.com>
2024-05-15 18:06:24 +0900

    style

--- packages/eslint-config/nextjs.js
+++ packages/eslint-config/nextjs.js
@@ -3,22 +3,17 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'unused-imports'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'next/core-web-vitals',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next/core-web-vitals', 'prettier'],
   rules: {
     quotes: ['error', 'single'],
     'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
     'no-duplicate-imports': 'error',
     'no-unused-vars': 'error',
     'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
-    'no-restricted-imports': ['error', { patterns: ['../'] } ],
+    'no-restricted-imports': ['error', { patterns: ['../'] }],
     'unused-imports/no-unused-imports': 'error',
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
-  }
-}
+  },
+};
--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -22,28 +22,35 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier', 
+    'prettier',
   ],
   rules: {
     quotes: ['error', 'single'],
     'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
     'no-duplicate-imports': 'error',
     'no-unused-vars': 'error',
-    'no-use-before-define': ['error', { 'functions': false }],
+    'no-use-before-define': ['error', { functions: false }],
     'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
-    'no-restricted-imports': ['error', { patterns: ['../'] } ],
-		'unused-imports/no-unused-imports': 'error',
+    'no-restricted-imports': ['error', { patterns: ['../'] }],
+    'unused-imports/no-unused-imports': 'error',
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
     'react/react-in-jsx-scope': 'off',
-    'react/function-component-definition': ['error', {
-      namedComponents: 'function-declaration',
-    }],
-    'import/extensions': ['error', 'ignorePackages', {
-      ts: 'never',
-      tsx: 'never',
-    }],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
   settings: {
     'import/resolver': {
@@ -56,4 +63,4 @@ module.exports = {
       version: '18',
     },
   },
-}
+};
